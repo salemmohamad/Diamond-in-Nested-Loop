5ab4742016b637531d818f5c565c3539b3da1feb
Salem M.Salem <salemmohamad3@hotmail.com>
2020-07-07 02:10:54 +0200

    Add project files.

---
+++ diamond in nested loop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diamond_in_nested_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number ");
+            int x = int.Parse(Console.ReadLine());
+            int a, b, i, j,m,n;
+            for (a = 1; a <= x; a++)
+            {
+                for (i = 1; i <= (x - a); i++)
+                {
+                    Console.Write(" ");
+                }
+                for (j = 1; j <= (2 * a) - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+
+            for (b = 1; b <= x - 1; b++)
+            {
+                for (m = 1; m <= x - (x - b); m++)
+                {
+                    Console.Write(" ");
+                }
+                for (n = 1; n <= ((2*x)-1)-(2*b); n+=1)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
